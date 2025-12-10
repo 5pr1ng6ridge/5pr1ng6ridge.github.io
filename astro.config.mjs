c5949af35cb3e5b5c3acb4aad96e58613748912f
5pr1ng6ridge <1008654188616sb@gmail.com>
2025-12-11 05:36:26 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://5pr1ng6ridge.gitlab.io",
-	base: "/idge-blog",
+	site: "https://5pr1ng6ridge.github.io",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
